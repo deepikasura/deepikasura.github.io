bd52264186b1d675533ca5f1068527ab746f8c40
deepikasura <suradeepika2009@gmail.com>
2026-07-28 19:46:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ window.onload = function () {
     }, 2200);
 
 };
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 800);
+});
